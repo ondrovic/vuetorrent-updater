13a3bfe8bdda57df57a96ce156c33d88d93f5c60
Chris Ondrovic <ondrovic@gmail.com>
2024-07-10 19:19:29 -0400

    Updates
    internal/utils/system.go: Added comments, added CheckError func
    
    internal/utils/system_test.go: updated tests

--- internal/utils/system/system.go
+++ internal/utils/system/system.go
@@ -1,19 +1,32 @@
+// system.go
 package system
 
 import "os"
 
+// Exitor is an interface that defines the Exit function
 type Exitor interface {
 	Exit(code int)
 }
 
+// OSExiter is an implementation of Exitor that calls os.Exit
 type OSExiter struct{}
 
+// Exit exits the program with the given code
 func (e OSExiter) Exit(code int) {
 	os.Exit(code)
 }
 
+// DefaultExitor is the default implementation of Exitor
 var DefaultExitor Exitor = OSExiter{}
 
+// Exit calls the DefaultExitor's Exit method
 func Exit(code int) {
 	DefaultExitor.Exit(code)
+}
+
+// CheckError checks for an error and exits with the given code if an error is found
+func CheckError(err error, code int) {
+	if err != nil {
+		Exit(code)
+	}
 }
--- internal/utils/system/system_test.go
+++ internal/utils/system/system_test.go
@@ -1,28 +1,84 @@
+// system_test.go
 package system
 
 import (
+	"errors"
+	"os"
+	"os/exec"
 	"testing"
 )
 
+// MockExiter is a mock implementation of Exitor that does not call os.Exit
 type MockExiter struct {
-	Code int
+	Code   int
+	Called bool
 }
 
+// Exit sets the exit code but does not exit the program
 func (e *MockExiter) Exit(code int) {
 	e.Code = code
+	e.Called = true
 }
 
 func TestExit(t *testing.T) {
-	mockExiter := MockExiter{}
-	DefaultExitor = &mockExiter
+	// Replace the DefaultExitor with a MockExiter
+	mockExiter := &MockExiter{}
+	DefaultExitor = mockExiter
 
+	// Call the Exit function
 	Exit(1)
 
-	exp := 1
-	res := mockExiter.Code
-
-	if res != exp {
-		t.Errorf("expected exit code %d, got %d", exp, res)
+	// Check that the MockExiter recorded the correct exit code
+	if mockExiter.Code != 1 {
+		t.Errorf("expected exit code 1, got %d", mockExiter.Code)
 	}
-	
+}
+
+func TestCheckError(t *testing.T) {
+	// Replace the DefaultExitor with a MockExiter
+	mockExiter := &MockExiter{}
+	DefaultExitor = mockExiter
+
+	// Define an error to pass to CheckError
+	err := errors.New("test error")
+
+	// Call the CheckError function with the error
+	CheckError(err, 1)
+
+	// Check that the MockExiter recorded the correct exit code
+	if mockExiter.Code != 1 {
+		t.Errorf("expected exit code 1, got %d", mockExiter.Code)
+	}
+	if !mockExiter.Called {
+		t.Errorf("expected exit to be called")
+	}
+
+	// Call the CheckError function without an error
+	mockExiter.Code = 0
+	mockExiter.Called = false
+	CheckError(nil, 1)
+
+	// Check that the MockExiter did not call exit
+	if mockExiter.Code != 0 {
+		t.Errorf("expected exit code to remain 0, got %d", mockExiter.Code)
+	}
+	if mockExiter.Called {
+		t.Errorf("expected exit not to be called")
+	}
+}
+
+func TestOSExiter(t *testing.T) {
+	if os.Getenv("BE_CRASHER") == "1" {
+		DefaultExitor = OSExiter{}
+		Exit(1)
+		return
+	}
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestOSExiter")
+	cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return // we expect a non-zero exit code
+	}
+	t.Fatalf("process ran with err %v, want exit status 1", err)
 }
