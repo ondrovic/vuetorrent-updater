45c74dbba9f2802869d26a2bb7be4e787d9f02c5
Chris Ondrovic <ondrovic@gmail.com>
2024-07-10 19:02:24 -0400

    internal/utils/system Updates
    internal/utils/system.go: refactored so it could be tested
    
    internal/utils/system_test.go: created test

--- internal/utils/system/system.go
+++ internal/utils/system/system.go
@@ -2,6 +2,18 @@ package system
 
 import "os"
 
-func Exit(code int) {
+type Exitor interface {
+	Exit(code int)
+}
+
+type OSExiter struct{}
+
+func (e OSExiter) Exit(code int) {
 	os.Exit(code)
+}
+
+var DefaultExitor Exitor = OSExiter{}
+
+func Exit(code int) {
+	DefaultExitor.Exit(code)
 }
--- internal/utils/system/system_test.go
+++ internal/utils/system/system_test.go
@@ -1,3 +1,28 @@
 package system
 
-//TODO: Write Tests
+import (
+	"testing"
+)
+
+type MockExiter struct {
+	Code int
+}
+
+func (e *MockExiter) Exit(code int) {
+	e.Code = code
+}
+
+func TestExit(t *testing.T) {
+	mockExiter := MockExiter{}
+	DefaultExitor = &mockExiter
+
+	Exit(1)
+
+	exp := 1
+	res := mockExiter.Code
+
+	if res != exp {
+		t.Errorf("expected exit code %d, got %d", exp, res)
+	}
+	
+}
